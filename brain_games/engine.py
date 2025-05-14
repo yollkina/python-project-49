fad806b57935d380b60503a45617c930e6cabfe7
Iuliia Elkina <yollkina@mail.ru>
2025-05-15 02:46:02 +0300

    Add engine.py, utils.py, constants.py

---
+++ brain_games/engine.py
@@ -0,0 +1,28 @@
+import prompt
+
+from brain_games.constants import rounds
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+
+def start_game(instruction, get_question_and_answer):
+    name = welcome_user()
+    print(instruction) 
+    for _ in range(rounds):
+        question, correct_answer = get_question_and_answer()
+        player_answer = input(f'Question: {question}\nYour answer: ')
+        if player_answer != correct_answer:
+            print(f"'{player_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.\n"
+                  f"Let's try again, {name}!")
+            break
+        else:
+            print("Correct!")
+    else:
+        print(f"Congratulations, {name}!")
+
